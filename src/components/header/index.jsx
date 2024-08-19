8f2d60b3a93ff97d54ce92dde249e996b275cf82
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-08-19 23:49:30 +0100

    bundle change

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,65 +1,63 @@
 import React, { forwardRef, useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 import Magnetic from "../magnetic";
 import { Toggle_Menu } from "../../../utils/pen";
 import "./style.css";
 
 const Header = forwardRef(function index({ isActive }, ref) {
   const [initialized, setInitialized] = useState(false);
-  const [showOverlay, setShowOverlay] = useState(false);
 
   useEffect(() => {
     if (!initialized) {
+      // Initialize the Button_Nav state on first render
       Toggle_Menu();
       setInitialized(true);
     } else {
-      Toggle_Menu();
+      // Toggle when isActive changes
+      !isActive ? Toggle_Menu() : Toggle_Menu();
     }
   }, [isActive, initialized]);
 
   const handleClick = () => {
     Toggle_Menu();
-    setShowOverlay((prev) => !prev);
   };
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
-    if (isActive) {
-      setShowOverlay(true);
+    if (!isActive) {
+      const timer = setTimeout(() => {
+        setShowMenu(true);
+      }, 1000); // Show the menu after 4 seconds
+
+      // Clear the timeout if `isActive` becomes true before 4 seconds
+      return () => clearTimeout(timer);
     } else {
-      setShowOverlay(false);
+      setShowMenu(false);
     }
   }, [isActive]);
 
   return (
-    <div className="containerheader">
-      <AnimatePresence>
-        {showOverlay && (
-          <motion.div
-            className="overlay"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }} // Inline style to change background color
-            initial={{ x: "100%" }} 
-            animate={{ x: 0 }} 
-            exit={{ x: "100%" }} 
-            transition={{ duration: 0.5 }} 
-          ></motion.div>
-        )}
-      </AnimatePresence>
-      {!isActive ? (
-        <span key={isActive} className="menutext">
-          Menu
-        </span>
-      ) : (
-        <span></span>
-      )}
+    <div
+    className="containerheader"
+    >
+    
+    
+    {!isActive ? (
+  <span key={isActive} className="menutext"> Menu</span>
+) : (
+  <span></span>
+)}
 
-      <div className="containericon">
+   
+
+      <div className="containericon" >
         <Magnetic>
           <div
             className={`burger ${isActive ? "active" : ""}`}
             onClick={handleClick}
           >
             <div ref={ref} className="bounds" onClick={handleClick}></div>
-            <div className="btnoutcontainero" id="Button_Nav"></div>
+            <div className="btnoutcontainero" id="Button_Nav"></div>{" "}
+            {/* Moved outside the burger */}
           </div>
         </Magnetic>
       </div>
