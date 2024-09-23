dd0412a102ea8d3be7a1ee98077b4891df63c20f
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-23 10:15:06 +0100

    pushchanges

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -54,7 +54,7 @@ export default function SideBar() {
         >
           <Header ref={stickyElement} isActive={isActive} isHoverd={isHoverd} setIsHoverd={setIsHoverd}/>
           <Headersecond ref={stickyElement1} />
-          <StickyCursor stickyElement={stickyElement1} isHoverd={isHoverd} isActive={isActive}/>
+          <StickyCursor stickyElement={stickyElement1} isHoverd={isHoverd} isActive={isActive} />
 
           <StickyCursor stickyElement={stickyElement} isHoverd={isHoverd} isActive={isActive}/>
         </div>
